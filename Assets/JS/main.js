75178fd9f242e8a664dd570b31de9717cba9b136
el glioui <elgliouiyoucode@gmail.com>
2023-12-05 09:39:53 +0100

    dashboard propretaire

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,5 +1,34 @@
-// Initialization for ES Users
-import { Collapse, Ripple, initMDB } from "mdb-ui-kit";
 
-initMDB({ Collapse, Ripple });
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+       document.getElementById("contenue").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "e.php", true);
+    xhttp.send();
+  }
 
+  function graph(){
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+  }
